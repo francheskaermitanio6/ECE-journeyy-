3ba23f509da36a2bebfeedb348372104ba304ceb
Francheska Ermitanio <francheskaermitanio6@gmail.com>
2026-04-20 12:41:23 +0800

    Update hello-ece.py

--- hello-ece.py
+++ hello-ece.py
@@ -2,3 +2,6 @@ print("Hello World!")
 print("Ako si Francheska, future ECE")
 print("Goal ko: Mag-work sa TI Clark tapos abroad")
 print("Start date: April 19, 2026")
+
+print("Hello, Future TI Clark Engineer!")
+print("Francheska is learning Python")
