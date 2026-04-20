115b218238400a0ec191894520da1bd6f34fab1f
Francheska Ermitanio <francheskaermitanio6@gmail.com>
2026-04-20 18:26:38 +0800

    Create AboutMe.java

---
+++ AboutMe.java
@@ -0,0 +1,15 @@
+public class AboutMe {
+    public static void main(String[] args) {
+        String name = "Francheska";
+        String age = "16";
+        String grade = "Grade 11";
+        String future = "ECE Engineer at TI Clark";
+        
+        System.out.println("=== ABOUT ME - JAVA VERSION ===");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Grade: " + grade);
+        System.out.println("Future: " + future);
+        System.out.println("\nKaya ko na mag-Java!");
+    }
+}
