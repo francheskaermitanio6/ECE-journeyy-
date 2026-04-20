f025120b5f0b41d174e8e16819dfa77127f2d73f
Francheska Ermitanio <francheskaermitanio6@gmail.com>
2026-04-20 18:11:26 +0800

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,20 @@
+print("=== FRANCHESKA'S CALCULATOR===")
+print ("Hi! Ako si Python. Magko-compute tayo!")
+
+# Step 1: TANONG
+num1 = int (input( "Bigay ka first number: "))
+num2 = int (input( "Bigay ka second number: "))
+
+# Step 2: Isip
+total = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+
+# Step 3: Sagot
+print("\n- - - RESULTA - - -")
+print(num1, "+", num2, "=", total)
+print(num1, "-", num2 ,"=", difference)
+print(num1, "x", num2 ,"=", product)
+print(num1, "÷", num2, "=" ,quotient)
+print("\nTapos na! Galing mo Francheska! ")
