7ca9daca2133d1595030345e0a0332dd73cb13bf
Francheska Ermitanio <francheskaermitanio6@gmail.com>
2026-04-21 12:47:16 +0800

    Create Francheska.py

---
+++ Francheska.py
@@ -0,0 +1,14 @@
+# Francheska OS v2.0 🚀
+> "Puro Error, Pero Survivor!"
+
+First solo Python project coded on mobile via Pydroid 3.
+40 minutes. 6 errors. 0 surrenders.
+
+### Features
+- User input & conditionals
+- Debugged IndentationError, SyntaxError, variable cases
+- Dedicated to family 🫶
+
+### Run it
+```python
+python francheska_os_v2.py
