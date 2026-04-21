131c7bad9b1f37e802ba5366c84d78d4906f48dc
Francheska Ermitanio <francheskaermitanio6@gmail.com>
2026-04-21 12:57:41 +0800

    Update Francheska.py

--- Francheska.py
+++ Francheska.py
@@ -22,3 +22,35 @@ Dream Board passer ng ECE detected ✅
 OMSIM! KAYA MO YAN
 
 
+
+# Francheska's Another Code - future ECE
+print("= = = Francheska OS v2.0 = = =")
+print("keri na to future ECE...")
+
+print("\npara kay mama")
+print("para kay papa")
+print("para kay tatang")
+
+name = input("\nAno name mo? ")
+dream = input("dream mo? ")
+kaya = input("Kaya pa today? ")
+
+print("\n- - -SYSTEM BOOT COMPLETE- - -")
+print(f"Hello {name}!")
+print(f"Dream {dream} detected ✅")
+print(f"Kaya pa today: {kaya}")
+
+if kaya.lower() == "ayoko na":
+    print("\nAdvice: Pahinga ka muna kaya mo yan!")
+elif kaya.lower() == "kakayanin":
+    print("OMSIM! KAYA MO YAN")
+else:
+    print("Kahit ano pa yan, kaya mo yan!")
+
+print("\nDebugged by: Francheska")
+print("Status: Puro Error, Pero Survivor!")
+       
+
+ 
+
+
